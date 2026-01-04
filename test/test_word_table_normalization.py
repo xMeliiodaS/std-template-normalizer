a87ca0cda7d24327f201689d53ac598618f284d8
xMeliiodaS <yourgithub@email.com>
2026-01-04 21:17:11 +0200

    Changed the main file name

--- test/test_word_table_normalization.py
+++ test/test_word_table_normalization.py
@@ -8,14 +8,14 @@ from src.word.table_handler import (set_landscape_for_all_sections,
                                     set_paragraph_spacing)
 
 
-class TestExcelViolations(unittest.TestCase):
+class TestWordTableNormalization(unittest.TestCase):
     def setUp(self):
         self.config = ConfigProvider.load_config_json()
         self.exported_word = self.config["Exported_word"]
         self.template_ready_word = self.config["Template_word"]
         self.output_word = self.config["Output_word"]
 
-    def test_excel_violations(self):
+    def test_word_table_normalization(self):
         """Validate that Excel is consistent and generate violations HTML report."""
         set_landscape_for_all_sections(self.exported_word, self.output_word)
 
@@ -46,5 +46,5 @@ if __name__ == "__main__":
     config = ConfigProvider.load_config_json(config_path)
 
     suite = unittest.TestSuite()
-    suite.addTest(TestExcelViolations('test_excel_violations'))
+    suite.addTest(TestWordTableNormalization('test_word_table_normalization'))
     unittest.TextTestRunner(verbosity=2).run(suite)
